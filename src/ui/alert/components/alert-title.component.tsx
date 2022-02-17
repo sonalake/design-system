8d17c58f38aaac982b79b38795b24376f863ac33
Michael Gray <michael.gray@sonalake.com>
2022-02-17 09:45:13 +0000

    feat(alert): add typography with ref component

--- src/ui/alert/components/alert-title.component.tsx
+++ src/ui/alert/components/alert-title.component.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { Title } from '@radix-ui/react-alert-dialog';
-import clsx from 'clsx';
 
 import { DialogHeader, DialogHeaderProps } from '../../dialog';
-import { getTypographyVariantStyles } from '../../typography/typography.styles';
+import { TypographyWithRef } from '../../typography';
 
 export const AlertTitle = ({ children, icon }: DialogHeaderProps) => (
   <DialogHeader icon={icon}>
     <Title asChild>
-      <h2 className={clsx(getTypographyVariantStyles('heading2'), 'font-bold')}>
-        {children}
-      </h2>
+      <TypographyWithRef variant="heading2">{children}</TypographyWithRef>
     </Title>
   </DialogHeader>
 );
