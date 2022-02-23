5c639587001281c959ab05afe90d5526eb878317
Kacper Kruczek <>
2022-02-23 12:54:27 +0100

    feat(button): added story for button component

---
+++ src/ui/button/button.stories.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { DefaultTestProviders } from '../../utils';
+import { Typography } from '../typography';
+import { Button } from './button.component';
+
+export default {
+  title: 'Atoms/Button',
+  component: Button,
+  decorators: [
+    (Story) => (
+      <DefaultTestProviders>
+        <Story />
+      </DefaultTestProviders>
+    ),
+  ],
+  argTypes: {
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+} as ComponentMeta<typeof Button>;
+
+export const Default: ComponentStory<typeof Button> = (args) => (
+  <Button {...args} onClick={() => alert()}>
+    <Typography translationKey="STORY_BOOK.BUTTON" />
+  </Button>
+);
+
+export const Small: ComponentStory<typeof Button> = () => (
+  <Button size="sm">
+    <Typography translationKey="STORY_BOOK.BUTTON" />
+  </Button>
+);
+
+export const Disabled: ComponentStory<typeof Button> = () => (
+  <Button disabled>
+    <Typography translationKey="STORY_BOOK.BUTTON" />
+  </Button>
+);
+
+export const WithIcons: ComponentStory<typeof Button> = () => (
+  <div className="inline-flex flex-col gap-2">
+    <Button>
+      <Typography translationKey="STORY_BOOK.BUTTON_RIGHT" />
+      <FaSearch />
+    </Button>
+    <Button>
+      <FaSearch />
+      <Typography translationKey="STORY_BOOK.BUTTON_LEFT" />
+    </Button>
+    <Button>
+      <FaSearch />
+      <Typography translationKey="STORY_BOOK.BUTTON_BOTH" />
+      <FaSearch />
+    </Button>
+  </div>
+);
