15a1548cfb796feac045bca45cfee4388270cee2
Michael Gray <michael.gray@sonalake.com>
2022-02-24 14:33:47 +0000

    feat(notifications): update stories

--- src/ui/notification/notification.stories.tsx
+++ src/ui/notification/notification.stories.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
-import { useNotifications } from '../../context/notifications';
 import { DefaultTestProviders } from '../../utils';
-import { Button } from '../button';
 import { Stack } from '../stack';
 import { Typography } from '../typography';
 import { Notification } from './notification.component';
@@ -22,32 +20,23 @@ export default {
 } as ComponentMeta<typeof Notification>;
 
 export const Default: ComponentStory<typeof Notification> = () => {
-  const notifications: [string, NotificationStatus][] = [
-    ['STORY_BOOK.NOTIFICATION.SUCCESS', 'success'],
-    ['STORY_BOOK.NOTIFICATION.ERROR', 'error'],
-    ['STORY_BOOK.NOTIFICATION.WARNING', 'warning'],
-    ['STORY_BOOK.NOTIFICATION.INFO', 'info'],
-    ['STORY_BOOK.NOTIFICATION.DEFAULT', 'default'],
+  const statuses: NotificationStatus[] = [
+    'success',
+    'error',
+    'warning',
+    'info',
+    'default',
   ];
-
-  const { addNotification } = useNotifications();
 
   return (
     <Stack vertical spacing={4} items="start">
-      {notifications.map(([translationKey, status]) => (
-        <Typography
-          key={status}
-          as={Button}
-          translationKey={translationKey}
-          className="mb-1"
-          onClick={() =>
-            addNotification({
-              message: (
-                <Typography translationKey="STORY_BOOK.NOTIFICATION.CONTENT" />
-              ),
-              status,
-            })
+      {statuses.map((status) => (
+        <Notification
+          message={
+            <Typography translationKey="STORY_BOOK.NOTIFICATION.CONTENT" />
           }
+          status={status}
+          onClear={() => {}}
         />
       ))}
     </Stack>
